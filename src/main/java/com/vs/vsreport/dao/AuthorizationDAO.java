1ad63e743666550f81f450ccdbb8eaeb8f7d00cb
avi3s <avirup.pal@gmail.com>
2020-10-27 19:13:05 +0530

    entity changes

--- src/main/java/com/vs/vsreport/dao/AuthorizationDAO.java
+++ src/main/java/com/vs/vsreport/dao/AuthorizationDAO.java
@@ -1,9 +1,9 @@
-package com.vs.vsreport.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.vs.vsreport.entity.AuthorizationEntity;
-
-public interface AuthorizationDAO extends CrudRepository<AuthorizationEntity, Long> {
-
-}
+//package com.vs.vsreport.dao;
+//
+//import org.springframework.data.repository.CrudRepository;
+//
+//import com.vs.vsreport.entity.AuthorizationEntity;
+//
+//public interface AuthorizationDAO extends CrudRepository<AuthorizationEntity, Long> {
+//
+//}
